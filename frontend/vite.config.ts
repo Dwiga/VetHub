c4cb5e51577e75d95e68864819c623b6e556ecd1
Ahmad Dwi <ahmad.dwi@rga.com>
2026-06-03 07:46:35 +0700

    #68 dev env config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   resolve: {
@@ -18,6 +19,7 @@ export default defineConfig({
   },
   plugins: [
     tanstackStart(),
+    netlify(),
     viteReact(),
     tailwindcss(),
   ],
